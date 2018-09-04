6527d2ef2942ce353ebf80353a34ddb39444306d
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-09-04 14:23:16 +0200

    Create project with TextInput Tuto

---
+++ WeatherTrip/TextInput.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class TextInput extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            saisie: "Saisir un texte..."
+        }
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput style={{
+                    height: 20,
+                    width: 200,
+                    borderWidth: 1}}
+                           value={this.state.saisie}
+                           onChangeText=
+                               {(text) => {this.setState({
+                                   saisie: text
+                               })
+                               }}
+                />
+
+                <Button title={"go"} onPress={() => {
+                    alert(this.state.saisie);
+                }}/>
+            </View>
+    );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
