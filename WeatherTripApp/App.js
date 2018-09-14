5b263cb518fdd1eb00b380a34136955041711448
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-09-14 15:20:33 +0200

    add splashscreen

--- WeatherTripApp/App.js
+++ WeatherTripApp/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Animated} from 'react-native';
 import Menu from "./components/Menu";
 import MeteoAPI from "./components/MeteoAPI";
 import SplashScreen from './components/SplashScreen';
 
+
 export default class App extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            isVisible : true,
+        }
+    }
+
+    componentDidMount(){
+        var self = this;
+
+        setTimeout(function(){
+            self.setState({ isVisible : false });
+        }, 2000);
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                {/* <SplashScreen/> */}
-                <Menu/>
+                {/* <SplashScreen/>
+                <Menu/> */}
+                { (this.state.isVisible === true) ? <SplashScreen/>  : <Menu/> }
+                {/* <View style={{backgroundColor: 'red'}}></View>
+                <View style={{backgroundColor: 'blue'}}></View> */}
             </View>
         );
     }
